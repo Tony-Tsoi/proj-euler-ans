07adb2f2d051d097ffb8d0c828a8bba42759fe99
Tony Tsoi <tsoiwaicheong@gmail.com>
2017-01-08 13:41:43 -0800

    added some solves

---
+++ prob4.py
@@ -0,0 +1,27 @@
+"""
+Problem 4
+
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+ # checks if the number is palindrome
+def isPalindrome(num):
+  aString = str(num)
+  length = len(aString)
+  for i in range(int(length / 2)):
+    if aString[i] != aString[length - i - 1]:
+      return False
+  return True
+  
+lPal = 0
+  
+ # for 2 three-digit number, i and j
+for i in range(100, 999):
+  for j in range(100, 999):
+    product = i * j
+    if isPalindrome(product) and product > lPal:
+      lPal = product
+      print('newPal ' + str(lPal))
+
+print(lPal)
---
+++ prob9.py
@@ -0,0 +1,17 @@
+"""
+Problem 9
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+"""
+for i in range(1, 500):
+  for j in range(i, 500):
+    for k in range(j, 1000):
+      if (i*i + j*j == k*k) and (i+j+k == 1000):
+        print(i, j, k)
+        print(i*j*k)
